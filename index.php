1ef229f4d158af0bf2eeab1cd52f2eb70244f69c
unknown <surya.neu71@gmail.com>
2020-07-23 22:41:56 +0545

    navbar added

--- index.php
+++ index.php
@@ -5,11 +5,73 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script src="js/jquery-3.5.1.min.js"></script>
 	<script src="https://unpkg.com/@popperjs/core@2"></script>
 	<script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-	<h1>Shree Deep Narayan Adarsh Ma</h1>
+<!-- A grey horizontal navbar that becomes vertical on small screens -->
+	<nav class="navbar navbar-expand-sm  navbar-dark" id="cust-nav">
+
+		<div class="container">
+			
+			<a class="navbar-brand" href="#">
+				<img src="img/logo2.png" alt="Logo" style="width:80px;">
+				<div class="sc-name-ctr">
+					<p class="sc-name">Shree Deep Naryan</p>
+					<p class="sc-name">Adarsha Ma Vi</p>
+				</div>
+			</a>
+
+			  <!-- Toggler/collapsibe Button -->
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+			    <!-- <span class="navbar-toggler-icon"></span> -->
+			    <span class="btn-toggler-icon"></span>
+			    <span class="btn-toggler-icon"></span>
+			    <span class="btn-toggler-icon"></span>
+			  </button>
+
+				<!-- Navbar links -->
+				<div class="collapse navbar-collapse flex-row-reverse" id="collapsibleNavbar">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+						  <a class="nav-link" href="#">Home</a>
+						</li>
+						<li class="nav-item">
+						  <a class="nav-link" href="#">About Us</a>
+						</li>
+						<li class="nav-item">
+						  <a class="nav-link" href="#">Gallery</a>
+						</li>
+						<li class="nav-item">
+						  <a class="nav-link" href="#">Alumnai</a>
+						</li>
+						<li class="nav-item">
+						  <a class="nav-link" href="#">Notice</a>
+						</li>
+						<li class="nav-item">
+						  <a class="nav-link" href="#">News & Events</a>
+						</li>
+						<li class="nav-item">
+						  <a class="nav-link" href="#">Contact</a>
+						</li>
+						<!-- Dropdown -->
+<!-- 						<li class="nav-item dropdown">
+						  <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+						    Dropdown link
+						  </a>
+						  <div class="dropdown-menu cust-drop">
+						    <a class="dropdown-item" href="#">Link 1</a>
+						    <a class="dropdown-item" href="#">Link 2</a>
+						    <a class="dropdown-item" href="#">Link 3</a>
+						  </div>
+						</li> -->
+					</ul>
+			</div>
+			
+		</div>
+
+	</nav> 
 </body>
 </html>
