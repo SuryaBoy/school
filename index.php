f5d30a837d27608e1dff55378d77738e656f49a0
unknown <surya.neu71@gmail.com>
2020-07-23 11:56:20 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shree Deep Narayan Adarsha Ma Vi</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="js/jquery-3.5.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<h1>Shree Deep Narayan Adarsh Ma</h1>
+</body>
+</html>
