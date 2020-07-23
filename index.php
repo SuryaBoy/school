9504662d5392fb4d6ee97f10287e0fcd42e3c4bb
unknown <surya.neu71@gmail.com>
2020-07-23 12:06:35 +0545

    popper cdn added

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<script src="js/jquery-3.5.1.min.js"></script>
+	<script src="https://unpkg.com/@popperjs/core@2"></script>
 	<script src="js/bootstrap.min.js"></script>
 </head>
 <body>
